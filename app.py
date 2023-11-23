44da22472b9a05c53106a6ce8ddbb49d7ec628a0
Felix Grzelka <felix.grzelka@hpi.de>
2023-11-23 20:20:11 +0100

    add code

---
+++ app.py
@@ -0,0 +1,93 @@
+import uuid
+from flask import Flask, jsonify, request, url_for
+from enum import Enum, auto
+import io
+import requests
+from flask import Flask, render_template
+from enum import Enum, auto
+import draw_image
+from PIL import Image
+
+app = Flask(__name__)
+
+AP_IP = "192.168.178.239"
+
+class DisplaySize(Enum):
+    SMALL = auto()
+    MEDIUM = auto()
+    LARGE = auto()
+    EXTRA_LARGE = auto()
+
+    @property
+    def size(self):
+        _sizes_map = {
+            DisplaySize.SMALL: (152, 152),
+            DisplaySize.MEDIUM: (296, 128),
+            DisplaySize.LARGE: (400, 300),
+            DisplaySize.EXTRA_LARGE: (640, 384),
+        }
+        return _sizes_map[self]
+
+
+def upload_image(image_path:str, display_size: DisplaySize, display_mac:str, ap_url:str, dither=False):
+    rgb_image = Image.open(image_path)
+    rgb_image = rgb_image.convert("RGB")
+    # rgb_image = rgb_image.resize(display_size.size)
+    buffer = io.BytesIO()
+    rgb_image.save(buffer, "JPEG", quality="maximum")
+    rgb_image.save("testing.jpg", "JPEG", quality="maximum")
+    url = "http://" + ap_url + "/imgupload"
+    
+    display_mac = display_mac.zfill(16)
+    payload = {"dither": 1 if dither else 0, "mac": display_mac.upper()}
+    response = requests.post(url, data=payload, files={"file": buffer.getvalue()})
+    print(response.text)
+    # response.raise_for_status()
+    return jsonify({"message": response.text, "file_name": image_path})
+    # return response.text
+
+@app.route("/")
+def index():
+    return render_template(
+        "index.html",
+        bootstrap_css=url_for("static", filename="bootstrap.min.css"),
+        bootstrap_js=url_for("static", filename="bootstrap.min.js"),
+        jquery_js=url_for("static", filename="jquery.min.js"),
+        example_image=url_for("static", filename="example.jpg"),
+    )
+
+@app.route('/image_upload', methods=['POST'])
+def image_upload():
+    print("image_upload")
+    # Get the uploaded file from the request
+    file = request.files['file']
+
+    # Save the file to a temporary location
+    temp_file_path = f"static/user/{uuid.uuid4().hex}.jpg"
+    file.save(temp_file_path)
+
+    mac_address = request.form.get('macAddress')
+    display_size = DisplaySize[request.form["size"].upper()]
+
+    return upload_image(temp_file_path, display_size, mac_address, AP_IP)
+
+
+@app.route('/upload', methods=['POST'])
+def generate_image():
+    # Get the data from the POST request
+    data = request.get_json()
+
+    # Extract the name, pronouns, and mac address from the data
+    name = data['nickname']
+    pronouns = data['pronouns']
+    mac_address = data['macAddress']
+    display_size = DisplaySize[data["size"].upper()]
+    font_path = f"{data['font']}.ttf"
+    print(name, pronouns, mac_address)
+    file_name = f"static/user/{uuid.uuid4().hex}.jpg"
+    draw_image.generate_image(name, pronouns, file_name, font_path)
+    return upload_image(file_name, display_size, mac_address, AP_IP)
+
+
+if __name__ == '__main__':
+    app.run()
--- draw_image.py
+++ draw_image.py
@@ -1,49 +1,72 @@
-from enum import Enum, auto
-import io
-import requests
 from PIL import Image, ImageDraw, ImageFont
 
-image = Image.new('P', (296, 128))
 
-palette = [
-    255, 255, 255,  # white
-    0, 0, 0,        # black
-    255, 0, 0       # red
-]
+def generate_image(name, pronouns, file_name, font_path):
+    RED = ( 255, 0, 0 )
+    WHITE = ( 255, 255, 255 )
+    BLACK = ( 0, 0, 0 )
+    font_path = "agency_fb.ttf"
+    image = Image.open("cccp-template.jpg")#.convert(mode="P", colors=3)
 
-# # Assign the color palette to the image
-image.putpalette(palette)
+    # # # # Initialize the drawing context
+    draw = ImageDraw.Draw(image)
 
-# # Initialize the drawing context
-draw = ImageDraw.Draw(image)
+    # # # Define the text lines
+    line1 = name
+    # Höhe: 11-84px
+    # Breite: 10-172px
+    # 431a
 
-# # Define the text lines
-line1 = "Hallo :)"
-# line2 = "und selbst?"
+    # # # Define the fonts and sizes
+    # font_line2 = ImageFont.truetype(font_path, size=24)  # Change the font file and size as per your preference
+    x_top = 15
+    y_top = 15
+    target_width = 172-x_top -6
+    target_height = 84-y_top - 6
 
-# # line1 = "24"
-# # #line1 = ""
-# # line2 = ""
+    draw.rectangle((x_top, y_top, target_width + x_top, target_height + y_top),
+                fill=RED)
 
-# # Define the fonts and sizes
-font_line1 = ImageFont.truetype('comic.ttf', size=48)  # Change the font file and size as per your preference
-# font_line2 = ImageFont.truetype('comic.ttf', size=36)  # Change the font file and size as per your preference
+    font_size = 5
+    step_size = 1
+    text_width = 0
+    text_height = 0
+    while text_width < target_width and text_height < target_height:
+        font_line1 = ImageFont.truetype(font_path, size=font_size)  # Change the font file and size as per your preference
+        text_bbox_line1 = draw.textbbox((0, 0), line1, font=font_line1)
+        text_width = (text_bbox_line1[2] - text_bbox_line1[0])
+        text_height = text_bbox_line1[3] - text_bbox_line1[1]
+        font_size += step_size
 
-# # Calculate the text bounding boxes to get the text widths and heights
-text_bbox_line1 = draw.textbbox((0, 0), line1, font=font_line1)
-# text_bbox_line2 = draw.textbbox((0, 0), line2, font=font_line2)
+    font_size-=step_size
+    font_line1 = ImageFont.truetype(font_path, size=font_size)
+    text_bbox_line1 = draw.textbbox((0, 0), line1, font=font_line1)
+    text_width = text_bbox_line1[2] - text_bbox_line1[0]
+    text_height = text_bbox_line1[3] - text_bbox_line1[1]
 
-# # Calculate the text positions to center the lines horizontally
-text_position_line1 = ((image.width - (text_bbox_line1[2] - text_bbox_line1[0])) // 2, 15)
-# text_position_line2 = ((image.width - (text_bbox_line2[2] - text_bbox_line2[0])) // 2, 55)
+    print("textw, texth", text_width, text_height)
 
-# # Write the text on the image
-draw.text(text_position_line1, line1, fill=2, font=font_line1)  # Use palette index 1 for black color
-# draw.text(text_position_line2, line2, fill=1, font=font_line2)  # Use palette index 2 for red color
 
-# # Convert the image to 24-bit RGB
-rgb_image = image.convert('RGB')
+    # text_bbox_line2 = draw.textbbox((0, 0), line2, font=font_line2)
 
-# # Save the image as JPEG with maximum quality
+    # # Calculate the text positions to center the lines horizontally
+    # text_position_line1 = (10, 11)
+    res_x = ((target_width - text_width) // 2)
+    res_y = ((target_height - text_height) // 2)
+    print("res", res_x, res_y)
+    text_position_line1 = ( x_top + res_x - text_bbox_line1[0] , y_top + res_y - text_bbox_line1[1])
+    print(text_position_line1)
+    # text_position_line2 = ((image.width - (text_bbox_line2[2] - text_bbox_line2[0])) // 2, 100)
 
-rgb_image.save("image.jpg", "JPEG", quality="maximum")
+    # # Write the text on the image
+    draw.text(text_position_line1, line1, fill=BLACK, font=font_line1)  # Use palette index 1 for black color
+    draw.rectangle((x_top + res_x , y_top + res_y,
+                    text_bbox_line1[2] + x_top+res_x, text_bbox_line1[3] - text_bbox_line1[1] + y_top + res_y),
+                outline=BLACK)
+    # draw.text(text_position_line2, line2, fill=RED, font=font_line2)  # Use palette index 2 for red color
+
+    # # Convert the image to 24-bit RGB
+    rgb_image = image.convert('RGB')
+
+    # # Save the image as JPEG with maximum quality
+    rgb_image.save(file_name, "JPEG", quality="maximum")
