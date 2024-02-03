b695be3ae04c30e9ce603bd955d7533e4ef24545
Felix Grzelka <felix.grzelka@hpi.de>
2024-02-03 22:42:36 +0100

    allow uploading to multiple tags

--- tag_configurator/upload_image.py
+++ tag_configurator/upload_image.py
@@ -70,6 +70,7 @@ def send_image_via_station(
     display_mac: str,
     port: str,
     dither: bool = False,
+    any_mac: bool = False,
 ):
     """Upload an image to the access point."""
     rgb_image = image.convert("RGB")
@@ -81,16 +82,17 @@ def send_image_via_station(
         )
 
     def get_image(mac):
-        if expand_mac(mac) == expand_mac(display_mac):
+        if any_mac or expand_mac(mac) == expand_mac(display_mac):
+            # TODO: check if tag supports BW or BWR
             return image2tag_format(
                 rgb_image,
-                DataType.BLACK_RED,
-            ), DataType.BLACK_RED
+                DataType.BLACK,
+            ), DataType.BLACK
         return None, None
 
     def upload_successful(ap, mac):
         print(f"Upload successful for {mac}")
-        ap.enabled = False
+        ap.enabled = any_mac
     
     
     access_point = AccessPoint(get_image=get_image, upload_successful=upload_successful, serial_port=port)
@@ -112,19 +114,21 @@ def upload_image_from_path(
 @click.option("-i", "--ip", default=None, help="The access points IP address")
 @click.option("-p", "--port", show_default=True, default="/dev/ttyACM0", help="The Zigbee Sticks serial port")
 @click.option("-d", "--dither", is_flag=True, show_default=True, default=False)
+@click.option("-a", "--any-mac", is_flag=True, show_default=True, default=False)
+@click.option("-v", "--verbose", is_flag=True, show_default=True, default=False)
 @click.command()
-def main(ip, mac, image_path, dither, port):
+def main(ip, mac, image_path, dither, port, any_mac, verbose):
     """Upload an image to the access point."""
     # while True:
     # mac = input("input mac to upload image: ")
-    logging.basicConfig(format="%(asctime)s - %(name)s - [%(levelname)s] - %(message)s")
+    logging.basicConfig(format="%(asctime)s - %(name)s - [%(levelname)s] - %(message)s", level=logging.DEBUG if verbose else logging.INFO)
     try:
         if ip is not None:
             response = upload_image_from_path(image_path, mac, ip, dither=dither)
             print(response.content.decode("utf-8"))
         else:
             print("upload directly")
-            send_image_via_station(Image.open(image_path), mac, dither=dither,port=port)
+            send_image_via_station(Image.open(image_path), mac, dither=dither,port=port, any_mac=any_mac)
 
     except ConnectionError:
         print("Could not connect to the access point")
