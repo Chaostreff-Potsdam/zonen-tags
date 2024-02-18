df7bd3db274965d824aa9b8136d0e660c5efaefb
Felix Grzelka <felix.grzelka@hpi.de>
2024-02-18 17:05:08 +0100

    automatic firmware update

--- tag_configurator/update_firmware.py
+++ tag_configurator/update_firmware.py
@@ -1,26 +1,79 @@
 #! python3
 """This script uploads new firmware to a tag."""
 
+import hashlib
 import logging
 import click
-from tag_configurator.libap import AccessPoint, load_firmware
-from tag_configurator.proto_def import DataType
-from tag_configurator.upload_image import expand_mac
+from .libap import AccessPoint, load_firmware
+from .proto_def import DataType
+from .upload_image import expand_mac
+import requests
+from pathlib import Path
+
+
+def get_firmware_type(firmware_path: str) -> DataType:
+    """Get the type of the firmware."""
+    firmware_path = Path(firmware_path)
+    firmware_name = firmware_path.name
+    return "_".join(firmware_name.split("_")[:-1])
+
+
+def get_firmware_versions():
+    url = "https://raw.githubusercontent.com/jjwbruijn/OpenEPaperLink/master/binaries/Tag/tagotaversions.json"
+    ota_versions = requests.get(url).json()
+    return ota_versions[0]
+
+
+def get_firmware4type(firmware_entry):
+    base_url = (
+        "https://raw.githubusercontent.com/jjwbruijn/OpenEPaperLink/master/binaries/Tag"
+    )
+    url = f"{base_url}/{firmware_entry['type']}_{firmware_entry['version']}.bin"
+    binary = requests.get(url).content
+    checksum = hashlib.md5(binary).hexdigest()
+    if checksum != firmware_entry["md5"]:
+        raise ValueError(
+            f"Checksum mismatch for {url}. Expected {firmware_entry['md5']}, got {checksum}."
+        )
+    return binary
 
 
 def upload_firmware_via_station(
-    firmware_path: str,
-    display_mac: str,
-    port: str
+    firmware_path: str, display_mac: str, port: str, ignore_type_check: bool = False
 ):
     """Upload an image to the access point."""
-    firmware = load_firmware(firmware_path)
+    firmware_versions = get_firmware_versions()
+
     loop = len(display_mac) == 0
     expanded_macs = [expand_mac(mac) for mac in display_mac]
 
-    def get_image(mac):
+    if firmware_path is not None:
+        firmware = load_firmware(firmware_path)
+        firmware_type = get_firmware_type(firmware_path)
+    else:
+        # TODO: download/cache all firmares to /tmp or somewhere
+        raise NotImplementedError("Firmware download not implemented yet.")
+        return
+
+    def get_image(mac, adr):
         if loop or expand_mac(mac) in expanded_macs:
-            return firmware, DataType.FIRMWARE_UPDATE
+            firmware_entry = firmware_versions[hex(adr.hwType)[2:].zfill(2).upper()]
+            hardware_type = firmware_entry["type"]
+
+            if firmware_path is None:
+                logging.warning("No firmware path provided.")
+                return None, None
+                # TODO cross check type with mac address
+                # TODO load firmware from local fs cache
+                return get_firmware4type(firmware_entry), DataType.FIRMWARE_UPDATE
+
+            if hardware_type == firmware_type or ignore_type_check:
+                return firmware, DataType.FIRMWARE_UPDATE
+            else:
+                logging.warning(
+                    f"Skipping {mac} because it is a different hardware type. Expected {firmware_type}, got {hardware_type}."
+                )
+                return None, None
         return None, None
 
     def upload_successful(ap, mac):
@@ -38,7 +91,7 @@ def upload_firmware_via_station(
 
 
 @click.argument("mac", nargs=-1)
-@click.argument("firmware_path")
+@click.option("-f", "--firmware", help="The path to the firmware file.")
 @click.option(
     "-p",
     "--port",
@@ -47,14 +100,19 @@ def upload_firmware_via_station(
     help="The Zigbee Sticks serial port",
 )
 @click.option("-v", "--verbose", is_flag=True, show_default=True, default=False)
+@click.option(
+    "-i", "--ignore-type-check", is_flag=True, show_default=True, default=False
+)
 @click.command()
-def main(firmware_path, mac, port, verbose):
+def main(firmware, mac, port, verbose, ignore_type_check):
     """Upload an image to the access point."""
     logging.basicConfig(
         format="%(asctime)s - %(name)s - [%(levelname)s] - %(message)s",
         level=logging.DEBUG if verbose else logging.INFO,
     )
-    upload_firmware_via_station(firmware_path, mac, port)
+    upload_firmware_via_station(
+        firmware, mac, port, ignore_type_check=ignore_type_check
+    )
 
 
 if __name__ == "__main__":
